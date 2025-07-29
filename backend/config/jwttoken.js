15fee4a4b73503f7428624078c081985104657d2
vinayak soni <147817292+Vinayaksoni123@users.noreply.github.com>
2025-07-29 15:35:06 +0530

    Update jwttoken.js

--- backend/config/jwttoken.js
+++ backend/config/jwttoken.js
@@ -3,7 +3,7 @@ import jwt from "jsonwebtoken";
 const getToken = async (userid) => {
   try {
     const token = jwt.sign({ userid }, process.env.JWT_SECRET, {
-      expiresIn: "2d",
+      expiresIn: "2h",
     });
     console.log(token);
     return token;
